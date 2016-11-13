657193bc61646ca5d535883550d96032d9767f18
ttjnguyen <ttjnguyen6@gmail.com>
2016-11-13 14:59:56 -0800

    Final commit

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *amountToPayTextField;
+@property (weak, nonatomic) IBOutlet UILabel *amountToPayPerPersonLabel;
+@property (weak, nonatomic) IBOutlet UISlider *numberOfPeopleSlider;
+
+
 
 @end
 
@@ -16,12 +21,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.amountToPayTextField.delegate = self;
+}
+- (IBAction)updateFromSlider:(id)sender {
+    
+    self.amountToPayPerPersonLabel.text = [self formatResultFromTotal:self.amountToPayTextField.text
+                                                            andPeople:self.numberOfPeopleSlider.value];
+    }
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    NSString *updatedString = [textField.text stringByReplacingCharactersInRange:range withString:string];
+    
+    
+    if(![updatedString isEqualToString:@""])
+        self.amountToPayPerPersonLabel.text = [self formatResultFromTotal:updatedString
+                                                                andPeople:self.numberOfPeopleSlider.value];
+    
+    return YES;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSString*)formatResultFromTotal:(NSString*)amountToPay andPeople:(int)numberOfPeople
+{
+    
+    NSDecimalNumber *number = [NSDecimalNumber decimalNumberWithString:amountToPay];
+    NSDecimalNumber *number2 = (NSDecimalNumber*)[NSDecimalNumber numberWithInteger:numberOfPeople];
+    NSDecimalNumber *number3= [number decimalNumberByDividingBy:number2];
+    
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    [formatter stringFromNumber:number3];
+    [formatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    [formatter stringFromNumber:number3];
+    
+    return [formatter stringFromNumber:number3];
 }
 
+- (IBAction)calculateSplitAmount:(id)sender {
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    
+    NSDecimalNumber* number = [NSDecimalNumber decimalNumberWithString:(self.amountToPayTextField.text)];
+    NSDecimalNumber *number2 = (NSDecimalNumber*)[NSDecimalNumber numberWithFloat:(self.numberOfPeopleSlider.value)];
+    NSDecimalNumber *number3= [number decimalNumberByDividingBy:number2];
+    
+    [formatter stringFromNumber:number3];
+    [formatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    self.amountToPayPerPersonLabel.text = [formatter stringFromNumber:number3];
+}
 @end
