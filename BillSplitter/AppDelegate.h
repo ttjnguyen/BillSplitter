9b28822ab48a3b342dc5f2506b1621533fdee715
ttjnguyen <ttjnguyen6@gmail.com>
2016-11-13 14:12:48 -0800

    Initial Commit

---
+++ BillSplitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BillSplitter
+//
+//  Created by Jenny Nguyen on 2016-11-13.
+//  Copyright © 2016 LightHouseLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
